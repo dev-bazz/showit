e98d44e04816c707a0b0d0d107d8a3997db7055d
Clement Bazuaye <clement.bazuaye@interswitchgroup.com>
2025-05-22 18:25:17 +0100

    Refactor project structure by removing unused components and simplifying layout files

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,19 +1,14 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { Stack } from 'expo-router';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen does not exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
+      <View style={styles.container}>
+        <Text style={styles.link}>This screen doesn't exist.</Text>
+      </View>
     </>
   );
 }
@@ -30,3 +25,4 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
   },
 });
+
